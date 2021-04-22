5b84fa04e4beac39c0f24f39f773a72f24740f7e
bruceyoy86 <bruced247@gmail.com>
2021-04-22 19:25:36 +0100

    messing around and making mistakes

--- src/test.js
+++ src/test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,12 +13,22 @@ const useStyles = makeStyles((theme) => ({
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
+  button: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  }
 }));
 
 export default function FullWidthGrid() {
   const classes = useStyles();
 
   return (
+    <>
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
@@ -43,5 +54,7 @@ export default function FullWidthGrid() {
         </Grid>
       </Grid>
     </div>
+    <Button className={classes.button}>Hook</Button>
+    </>
   );
 }
