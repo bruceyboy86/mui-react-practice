2b1916e6994442162b8361ec46c0edb66eeb13be
bruceyboy86 <bruced247@gmail.com>
2021-04-26 23:48:38 +0100

    nessing around with the theme

--- src/index.js
+++ src/index.js
@@ -10,15 +10,28 @@ const theme = createMuiTheme({
       main: "#556cd6",
     },
   },
-  props:{
-
-    h3: {
-      fontSize: '1.2rem',
-      '@media (min-width:600px)': {
-        fontSize: '1.5rem',
-      },
+  h3: {
+    fontSize: "1.2rem",
+    "@media (min-width:600px)": {
+      fontSize: "1.5rem",
     },
-  }
+  },
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: "1em",
+    textAlign: "center",
+  },
+  button: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+  },
 });
 
 ReactDOM.render(
--- src/test.js
+++ src/test.js
@@ -1,61 +1,42 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { useTheme } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-  button: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    border: 0,
-    borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    height: 48,
-    padding: '0 30px',
-  }
-}));
+
 
 export default function FullWidthGrid() {
-  const classes = useStyles();
-
+  const theme = useTheme();
   return (
     <>
-    <div className={classes.root}>
+    <div className={theme.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
+          <Paper className={theme.paper}>xs=12</Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+          <Paper className={theme.paper}>xs=12 sm=6</Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+          <Paper className={theme.paper}>xs=12 sm=6</Paper>
         </Grid>
         <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          <Paper className={theme.paper}>xs=6 sm=3</Paper>
         </Grid>
         <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          <Paper className={theme.paper}>xs=6 sm=3</Paper>
         </Grid>
         <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          <Paper className={theme.paper}>xs=6 sm=3</Paper>
         </Grid>
         <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          <Paper className={theme.paper}>xs=6 sm=3</Paper>
         </Grid>
       </Grid>
     </div>
-    <Button className={classes.button}>Hook</Button>
-    <h3 >testing h3</h3>
+    <Button className={theme.button}>Hook</Button>
+    <h3 className={theme.h3}>testing h3</h3>
     </>
   );
 }
